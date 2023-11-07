1ae58941ec0af2585d162c27c2f41bc96f6254c1
Sergey Ryazanov <sergey.ryazanov@nordigy.ru>
2023-11-07 14:17:54 +0100

    test ci

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use teloxide::{prelude::*, update_listeners::webhooks};
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting dice webhook bot...");
+    log::info!("Starting bot...");
 
     let bot = Bot::from_env();
 
